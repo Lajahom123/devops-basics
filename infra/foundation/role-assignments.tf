de437301d59907d09d946e383bc9298332249724
Atanas Atanasov <naskoat109@gmail.com>
2026-06-27 13:27:35 +0200

    restructure foundation role assignments

--- infra/foundation/role-assignments.tf
+++ infra/foundation/role-assignments.tf
@@ -1,3 +1,5 @@
+# Key Vault
+
 resource "azurerm_role_assignment" "current_deployer_key_vault_administrator" {
   scope                = module.key_vault.key_vault_id
   role_definition_name = "Key Vault Administrator"
@@ -8,4 +10,12 @@ resource "azurerm_role_assignment" "current_deployer_key_vault_data_access_admin
   scope                = module.key_vault.key_vault_id
   role_definition_name = "Key Vault Data Access Administrator"
   principal_id         = data.azurerm_client_config.current.object_id
+}
+
+# ACR
+
+resource "azurerm_role_assignment" "github_actions_acr_build_executor" {
+  scope              = module.acr.acr_id
+  role_definition_id = "AcrPush"
+  principal_id       = module.managed_identities.identities.github_actions_deploy.principal_id
 }
